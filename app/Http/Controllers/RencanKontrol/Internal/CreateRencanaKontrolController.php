46c8a18126729d0c80391e5d7b78f871d83a0c66
rezaandreannn <andreanreza042@gmail.com>
2023-11-04 13:22:09 +0700

    rencana kontrol internal

---
+++ app/Http/Controllers/RencanKontrol/Internal/CreateRencanaKontrolController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\RencanKontrol\Internal;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CreateRencanaKontrolController extends Controller
+{
+    public function create()
+    {
+    }
+}
---
+++ app/Models/RencanaKontrolInternal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RencanaKontrolInternal extends Model
+{
+    use HasFactory;
+}
