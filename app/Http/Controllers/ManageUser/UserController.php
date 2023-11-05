e798751b3b3c936977e8ac67f09dd80a3fb35316
itrsumm <itrsumm08@gmail.com>
2023-11-05 21:23:33 +0700

    merapihkan tampilan manage user

--- app/Http/Controllers/ManageUser/UserController.php
+++ app/Http/Controllers/ManageUser/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
     public function index()
     {
         //
-        $title = 'Role';
+        $title = 'User';
         $roles = Role::all();
         $users = User::with('roles')->get();
 
